d073b3efd8bea517bb2fad92547df0673e80f3f4
Francis Galiegue <fgaliegue@gmail.com>
2013-02-16 17:46:58 +0100

    TreePointerTest: add traversal tests as well
    Verify that traversal stops at the first token resolver which returns null.
    Verify that when a missing node is specified, it is returned on .path().

--- src/test/java/com/github/fge/jsonschema/jsonpointer/TreePointerTest.java
+++ src/test/java/com/github/fge/jsonschema/jsonpointer/TreePointerTest.java
@@ -1,5 +1,6 @@
 package com.github.fge.jsonschema.jsonpointer;
 
+import com.fasterxml.jackson.core.TreeNode;
 import com.github.fge.jsonschema.report.ProcessingMessage;
 import com.google.common.collect.ImmutableList;
 import org.testng.annotations.Test;
@@ -8,6 +9,7 @@ import java.util.List;
 
 import static com.github.fge.jsonschema.matchers.ProcessingMessageAssert.*;
 import static com.github.fge.jsonschema.messages.JsonPointerMessages.*;
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
 public final class TreePointerTest
@@ -72,5 +74,59 @@ public final class TreePointerTest
             = TreePointer.tokensFromInput("/a//b");
 
         assertEquals(actual, expected);
+    }
+
+    @Test
+    public void gettingTraversalResultGoesNoFurtherThanFirstMissing()
+    {
+        @SuppressWarnings("unchecked")
+        final TokenResolver<TreeNode> token1 = mock(TokenResolver.class);
+        @SuppressWarnings("unchecked")
+        final TokenResolver<TreeNode> token2 = mock(TokenResolver.class);
+        final TreeNode missing = mock(TreeNode.class);
+
+        when(token1.get(any(TreeNode.class))).thenReturn(null);
+
+        final DummyPointer ptr = new DummyPointer(missing,
+            ImmutableList.of(token1, token2));
+
+        final TreeNode node = mock(TreeNode.class);
+        final TreeNode ret = ptr.get(node);
+        verify(token1, only()).get(node);
+        verify(token2, never()).get(any(TreeNode.class));
+
+        assertNull(ret);
+    }
+
+    @Test
+    public void gettingPathOfMissingNodeReturnsMissingNode()
+    {
+        @SuppressWarnings("unchecked")
+        final TokenResolver<TreeNode> token1 = mock(TokenResolver.class);
+        @SuppressWarnings("unchecked")
+        final TokenResolver<TreeNode> token2 = mock(TokenResolver.class);
+        final TreeNode missing = mock(TreeNode.class);
+
+        when(token1.get(any(TreeNode.class))).thenReturn(null);
+
+        final DummyPointer ptr = new DummyPointer(missing,
+            ImmutableList.of(token1, token2));
+
+        final TreeNode node = mock(TreeNode.class);
+        final TreeNode ret = ptr.path(node);
+        verify(token1, only()).get(node);
+        verify(token2, never()).get(any(TreeNode.class));
+
+        assertSame(ret, missing);
+    }
+
+    private static final class DummyPointer
+        extends TreePointer<TreeNode>
+    {
+        DummyPointer(final TreeNode missing,
+            final List<TokenResolver<TreeNode>> tokenResolvers)
+        {
+            super(missing, tokenResolvers);
+        }
     }
 }
