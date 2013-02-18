2edc808cf6ebc7ba750efe9ed28dc2520fc8bed9
Francis Galiegue <fgaliegue@gmail.com>
2013-02-18 17:42:17 +0100

    Rudimentary javadoc for report package
    But it needs vast improvements first.

---
+++ src/main/javadoc/com/github/fge/jsonschema/report/package-info.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Processing report infrastructure
+ *
+ * <p>The four main classes of this package are:</p>
+ *
+ * <ul>
+ *     <li>{@link com.github.fge.jsonschema.report.ProcessingMessage} (an
+ *     individual message);</li>
+ *     <li>{@link com.github.fge.jsonschema.report.LogLevel} (the log level of a
+ *     message);</li>
+ *     <li>{@link com.github.fge.jsonschema.report.MessageProvider} (processing
+ *     inputs/outputs message providers);</li>
+ *     <li>{@link com.github.fge.jsonschema.report.ProcessingReport} (interface
+ *     to a processing report).</li>
+ * </ul>
+ */
+package com.github.fge.jsonschema.report;
