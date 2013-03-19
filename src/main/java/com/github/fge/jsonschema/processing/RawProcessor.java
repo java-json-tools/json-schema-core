be81e184cb877bb01dc19f334d200dae2468a3b7
Francis Galiegue <fgaliegue@gmail.com>
2013-03-19 19:03:16 +0100

    Add Javadoc to RawProcessor

--- src/main/java/com/github/fge/jsonschema/processing/RawProcessor.java
+++ src/main/java/com/github/fge/jsonschema/processing/RawProcessor.java
@@ -18,22 +18,51 @@
 package com.github.fge.jsonschema.processing;
 
 import com.github.fge.jsonschema.exceptions.ProcessingException;
+import com.github.fge.jsonschema.report.MessageProvider;
 import com.github.fge.jsonschema.report.ProcessingMessage;
 import com.github.fge.jsonschema.report.ProcessingReport;
 import com.github.fge.jsonschema.util.ValueHolder;
 
+/**
+ * Processor wrapper class
+ *
+ * <p>This class allows to declare a {@link Processor} with "raw" types, that
+ * is inputs and outputs which do not implement {@link MessageProvider}. Inputs
+ * and outputs are automatically wrapped into a {@link ValueHolder}.</p>
+ *
+ * <p>Implementations of this class are only required to provide a name by which
+ * the input and output will be identified in a processing message.</p>
+ *
+ * @param <IN> type of input
+ * @param <OUT> type of output
+ */
 public abstract class RawProcessor<IN, OUT>
     implements Processor<ValueHolder<IN>, ValueHolder<OUT>>
 {
     private final String inputName;
     private final String outputName;
 
+    /**
+     * Protected constructor
+     *
+     * @param inputName name of the input
+     * @param outputName name of the output
+     */
     protected RawProcessor(final String inputName, final String outputName)
     {
         this.inputName = inputName;
         this.outputName = outputName;
     }
 
+    /**
+     * Process a raw input, return a raw output
+     *
+     * @param report the report to use
+     * @param input the raw input
+     * @return the raw output
+     * @throws ProcessingException processing failure
+     * @see #newMessage(Object)
+     */
     protected abstract OUT rawProcess(final ProcessingReport report,
         final IN input)
         throws ProcessingException;
@@ -48,6 +77,12 @@ public abstract class RawProcessor<IN, OUT>
         return ValueHolder.hold(outputName, rawOutput);
     }
 
+    /**
+     * Create a new processing message for reporting purposes
+     *
+     * @param rawInput the raw input
+     * @return a new message
+     */
     protected final ProcessingMessage newMessage(final IN rawInput)
     {
         return new ProcessingMessage().put(inputName, rawInput);
