68503af37653d6fc9214fa3d1e1457c63d4ba4fc
Francis Galiegue <fgaliegue@gmail.com>
2013-03-28 22:02:15 +0100

    Start and implement hyper schema's "media" keyword
    Tests fail for inner contents of "media", however. On purpose.

---
+++ src/test/java/com/github/fge/jsonschema/syntax/checkers/hyperschema/draftv4/MediaTest.java
@@ -0,0 +1,13 @@
+package com.github.fge.jsonschema.syntax.checkers.hyperschema.draftv4;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public final class MediaTest
+    extends DraftV4HyperSchemaSyntaxCheckersTest
+{
+    public MediaTest()
+        throws JsonProcessingException
+    {
+        super("media");
+    }
+}
