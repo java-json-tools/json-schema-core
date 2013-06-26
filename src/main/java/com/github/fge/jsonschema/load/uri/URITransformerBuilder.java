e90d9d11c8be284d59c618ba6daf75f96ba86248
Francis Galiegue <fgaliegue@gmail.com>
2013-06-26 22:55:35 +0200

    URITransformerBuilder: implement all building code with argument checking etc
    Not tested yet.

--- src/main/java/com/github/fge/jsonschema/load/uri/URITransformerBuilder.java
+++ src/main/java/com/github/fge/jsonschema/load/uri/URITransformerBuilder.java
@@ -1,6 +1,10 @@
 package com.github.fge.jsonschema.load.uri;
 
 import com.github.fge.Thawed;
+import com.github.fge.jsonschema.messages.JsonSchemaCoreMessageBundle;
+import com.github.fge.jsonschema.ref.JsonRef;
+import com.github.fge.msgsimple.bundle.MessageBundle;
+import com.github.fge.msgsimple.serviceloader.MessageBundleFactory;
 import com.google.common.collect.Maps;
 
 import java.net.URI;
@@ -9,6 +13,9 @@ import java.util.Map;
 public final class URITransformerBuilder
     implements Thawed<URITransformer>
 {
+    private static final MessageBundle BUNDLE
+        = MessageBundleFactory.getBundle(JsonSchemaCoreMessageBundle.class);
+
     private static final URI EMPTY = URI.create("");
 
     URI namespace = EMPTY;
@@ -28,9 +35,88 @@ public final class URITransformerBuilder
         schemaRedirects.putAll(transformer.schemaRedirects);
     }
 
+    public URITransformerBuilder setNamespace(final URI uri)
+    {
+        BUNDLE.checkNotNull(uri, "uriTransform.nullInput");
+        namespace = toPathURI(uri);
+        return this;
+    }
+
+    public URITransformerBuilder setNamespace(final String uri)
+    {
+        BUNDLE.checkNotNull(uri, "uriTransform.nullInput");
+        namespace = toPathURI(URI.create(uri));
+        return this;
+    }
+
+    public URITransformerBuilder addSchemaRedirect(final URI from,
+        final URI to)
+    {
+        BUNDLE.checkNotNull(from, "uriTransform.nullInput");
+        BUNDLE.checkNotNull(to, "uriTransform.nullInput");
+        schemaRedirects.put(toSchemaURI(from), toSchemaURI(to));
+        return this;
+    }
+
+    public URITransformerBuilder addSchemaRedirect(final String from,
+        final String to)
+    {
+        BUNDLE.checkNotNull(from, "uriTransform.nullInput");
+        BUNDLE.checkNotNull(to, "uriTransform.nullInput");
+        final URI src = URI.create(from);
+        final URI dst = URI.create(to);
+        schemaRedirects.put(toSchemaURI(src), toSchemaURI(dst));
+        return this;
+    }
+
+    public URITransformerBuilder addPathRedirect(final URI from,
+        final URI to)
+    {
+        BUNDLE.checkNotNull(from, "uriTransform.nullInput");
+        BUNDLE.checkNotNull(to, "uriTransform.nullInput");
+        pathRedirects.put(toPathURI(from), toPathURI(to));
+        return this;
+    }
+
+    public URITransformerBuilder addPathRedirect(final String from,
+        final String to)
+    {
+        BUNDLE.checkNotNull(from, "uriTransform.nullInput");
+        BUNDLE.checkNotNull(to, "uriTransform.nullInput");
+        final URI src = URI.create(from);
+        final URI dst = URI.create(to);
+        pathRedirects.put(toPathURI(src), toPathURI(dst));
+        return this;
+    }
+
     @Override
     public URITransformer freeze()
     {
         return new URITransformer(this);
+    }
+
+    private static URI toSchemaURI(final URI uri)
+    {
+        final URI normalized = uri.normalize();
+        BUNDLE.checkArgumentPrintf(normalized.isAbsolute(),
+            "uriTransform.notAbsolute", uri);
+        final JsonRef ref = JsonRef.fromURI(normalized);
+        BUNDLE.checkArgumentPrintf(ref.isAbsolute(),
+            "uriTransform.notAbsoluteRef", uri);
+        return ref.getLocator();
+    }
+
+    private static URI toPathURI(final URI uri)
+    {
+        final URI normalized = uri.normalize();
+        BUNDLE.checkArgumentPrintf(normalized.isAbsolute(),
+            "uriTransform.notAbsolute", uri);
+        BUNDLE.checkArgumentPrintf(uri.getFragment() == null,
+            "uriTransform.fragmentNotNull", uri);
+        BUNDLE.checkArgumentPrintf(uri.getQuery() == null,
+            "uriTransform.queryNotNull", uri);
+        BUNDLE.checkArgumentPrintf(uri.getPath().endsWith("/"),
+            "uriTransform.noEndingSlash", uri);
+        return normalized;
     }
 }
