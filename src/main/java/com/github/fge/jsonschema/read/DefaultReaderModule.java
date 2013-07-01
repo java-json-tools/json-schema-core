935322127713f4971ac61e01682c8468548ab06a
Francis Galiegue <fgaliegue@gmail.com>
2013-07-01 12:14:26 +0200

    Experimental code for a new reader module/class

---
+++ src/main/java/com/github/fge/jsonschema/read/DefaultReaderModule.java
@@ -0,0 +1,47 @@
+package com.github.fge.jsonschema.read;
+
+import com.github.fge.jsonschema.load.Dereferencing;
+import com.google.common.annotations.Beta;
+
+import java.util.EnumSet;
+
+import static com.fasterxml.jackson.core.JsonParser.*;
+
+@Beta
+public final class DefaultReaderModule
+    extends ReaderModule
+{
+    /**
+     * Default JsonParser feature set. Unfortunately, Jackson does not use
+     * EnumSets to collect them, so we have to do that...
+     */
+    private static final EnumSet<Feature> DEFAULT_PARSER_FEATURES;
+
+    static {
+        DEFAULT_PARSER_FEATURES = EnumSet.noneOf(Feature.class);
+
+        for (final Feature feature: Feature.values())
+            if (feature.enabledByDefault())
+                DEFAULT_PARSER_FEATURES.add(feature);
+    }
+
+    private final Dereferencing dereferencing;
+    private final EnumSet<Feature> parserFeatures
+        = EnumSet.copyOf(DEFAULT_PARSER_FEATURES);
+
+    public DefaultReaderModule(final Dereferencing dereferencing,
+        final Feature... features)
+    {
+        this.dereferencing = BUNDLE.checkNotNull(dereferencing,
+            "loadingCfg.nullDereferencingMode");
+        for (final Feature feature: features)
+            parserFeatures.add(BUNDLE.checkNotNull(feature,
+                "loadingCfg.nullJsonParserFeature"));
+    }
+
+    @Override
+    protected SchemaReader newReader()
+    {
+        return new DefaultSchemaReader(dereferencing, parserFeatures);
+    }
+}
---
+++ src/main/java/com/github/fge/jsonschema/read/ReaderModule.java
@@ -0,0 +1,29 @@
+package com.github.fge.jsonschema.read;
+
+import com.github.fge.jsonschema.messages.JsonSchemaCoreMessageBundle;
+import com.github.fge.msgsimple.bundle.MessageBundle;
+import com.github.fge.msgsimple.load.MessageBundles;
+import com.google.common.annotations.Beta;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+@Beta
+public abstract class ReaderModule
+    extends AbstractModule
+{
+    protected static final MessageBundle BUNDLE
+        = MessageBundles.getBundle(JsonSchemaCoreMessageBundle.class);
+
+    @Override
+    protected final void configure()
+    {
+    }
+
+    @Provides
+    public final SchemaReader getReader()
+    {
+        return newReader();
+    }
+
+    protected abstract SchemaReader newReader();
+}
