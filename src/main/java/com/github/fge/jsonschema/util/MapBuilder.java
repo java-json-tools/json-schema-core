12c07670c507ac4bf05625269dc9f42072101d5d
Francis Galiegue <fgaliegue@gmail.com>
2013-07-03 18:26:05 +0200

    MapBuilder: add documentation

--- src/main/java/com/github/fge/jsonschema/util/MapBuilder.java
+++ src/main/java/com/github/fge/jsonschema/util/MapBuilder.java
@@ -10,6 +10,16 @@ import com.google.common.collect.Maps;
 
 import java.util.Map;
 
+/**
+ * A map builder with key/value/pair normalization and checking
+ *
+ * <p>Note that all map builders reject null keys or values.</p>
+ *
+ * @param <K> type of the keys
+ * @param <V> type of the values
+ *
+ * @since 1.1.9
+ */
 @Beta
 public abstract class MapBuilder<K, V>
 {
@@ -22,17 +32,42 @@ public abstract class MapBuilder<K, V>
     private final Function<V, V> valueNormalizer;
     private final ArgumentChecker<V> valueChecker;
 
+    /**
+     * Protected constructor
+     *
+     * @param keyNormalizer the key normalizer
+     * @param keyChecker the key checker
+     * @param valueNormalizer the value normalizer
+     * @param valueChecker the value checker
+     * @throws NullPointerException one normalizer or checker is null
+     */
     protected MapBuilder(final Function<K, K> keyNormalizer,
         final ArgumentChecker<K> keyChecker,
         final Function<V, V> valueNormalizer,
         final ArgumentChecker<V> valueChecker)
     {
-        this.keyNormalizer = keyNormalizer;
-        this.keyChecker = keyChecker;
-        this.valueNormalizer = valueNormalizer;
-        this.valueChecker = valueChecker;
+        this.keyNormalizer = BUNDLE.checkNotNull(keyNormalizer,
+            "mapBuilder.nullNormalizer");
+        this.keyChecker = BUNDLE.checkNotNull(keyChecker,
+            "mapBuilder.nullChecker");
+        this.valueNormalizer = BUNDLE.checkNotNull(valueNormalizer,
+            "mapBuilder.nullNormalizer");
+        this.valueChecker = BUNDLE.checkNotNull(valueChecker,
+            "mapBuilder.nullChecker");
     }
 
+    /**
+     * Put a key/value pair in the map builder
+     *
+     * <p>Both the keys and values are first normalized, then checked; finally,
+     * before insertion, the key/value pair is checked.</p>
+     *
+     * @param key the key
+     * @param value the value
+     * @return this
+     * @throws NullPointerException the key or value is null
+     * @throws IllegalArgumentException see {@link ArgumentChecker}
+     */
     public final MapBuilder<K, V> put(final K key, final V value)
     {
         BUNDLE.checkNotNull(key, "mapBuilder.nullKey");
@@ -50,6 +85,16 @@ public abstract class MapBuilder<K, V>
         return this;
     }
 
+    /**
+     * Put the contents from another map into this map builder
+     *
+     * <p>This calls {@link #put(Object, Object)} on each key/value pair in the
+     * map.</p>
+     *
+     * @param otherMap the map
+     * @return this
+     * @throws NullPointerException map is null
+     */
     public final MapBuilder<K, V> putAll(final Map<K, V> otherMap)
     {
         BUNDLE.checkNotNull(otherMap, "mapBuilder.nullMap");
@@ -60,6 +105,14 @@ public abstract class MapBuilder<K, V>
         return this;
     }
 
+    /**
+     * Build the map
+     *
+     * <p>The returned map is immutable.</p>
+     *
+     * @return a map
+     * @see ImmutableMap
+     */
     public final Map<K, V> build()
     {
         return ImmutableMap.copyOf(map);
