3c60e7d037aef8599f88a55b7c7e7196d492ab73
Francis Galiegue <fgaliegue@gmail.com>
2013-07-11 18:06:28 +0200

    Add SchemaDescriptors, SchemaSelectorModule
    Not tested yet.

---
+++ src/main/java/com/github/fge/jsonschema/keyword/SchemaSelectorModule.java
@@ -0,0 +1,69 @@
+package com.github.fge.jsonschema.keyword;
+
+import com.github.fge.jsonschema.messages.JsonSchemaCoreMessageBundle;
+import com.github.fge.msgsimple.bundle.MessageBundle;
+import com.github.fge.msgsimple.load.MessageBundles;
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+import java.net.URI;
+import java.util.Map;
+
+@Beta
+public class SchemaSelectorModule
+    extends AbstractModule
+{
+    private static final MessageBundle BUNDLE
+        = MessageBundles.getBundle(JsonSchemaCoreMessageBundle.class);
+
+    private final Map<URI, SchemaDescriptor> descriptors
+        = Maps.newHashMap();
+    private SchemaDescriptor defaultDescriptor;
+
+    public SchemaSelectorModule()
+    {
+        defaultDescriptor = SchemaDescriptors.byDefault();
+
+        SchemaDescriptor descriptor;
+
+        descriptor = SchemaDescriptors.draftv3();
+        descriptors.put(descriptor.getLocator(), descriptor);
+        descriptor = SchemaDescriptors.draftv4();
+        descriptors.put(descriptor.getLocator(), descriptor);
+        descriptor = SchemaDescriptors.draftv4HyperSchema();
+        descriptors.put(descriptor.getLocator(), descriptor);
+    }
+
+    public final void setDefaultDescriptor(final SchemaDescriptor descriptor)
+    {
+        defaultDescriptor = BUNDLE.checkNotNull(descriptor,
+            "schemaDescriptor.nullDescriptor");
+    }
+
+    public final void addDescriptor(final SchemaDescriptor descriptor)
+    {
+        final URI uri = descriptor.getLocator();
+        BUNDLE.checkArgumentPrintf(descriptors.put(uri, descriptor) == null,
+            "schemaSelector.duplicateSchema", uri);
+    }
+
+    @Override
+    protected final void configure()
+    {
+    }
+
+    @Provides
+    public final Map<URI, SchemaDescriptor> getDescriptors()
+    {
+        return ImmutableMap.copyOf(descriptors);
+    }
+
+    @Provides
+    public final SchemaDescriptor getDefaultDescriptor()
+    {
+        return defaultDescriptor;
+    }
+}
