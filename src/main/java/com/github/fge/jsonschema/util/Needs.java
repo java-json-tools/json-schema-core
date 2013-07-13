6e15248dc00fcc2eb4f0bf7203d0874febdbc690
Francis Galiegue <fgaliegue@gmail.com>
2013-07-13 13:27:30 +0200

    Add a @Needs annotation to document class dependencies

---
+++ src/main/java/com/github/fge/jsonschema/util/Needs.java
@@ -0,0 +1,15 @@
+package com.github.fge.jsonschema.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Needs
+{
+    Class<?>[] value();
+}
