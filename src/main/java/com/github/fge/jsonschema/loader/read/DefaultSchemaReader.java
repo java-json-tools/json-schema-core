9d64450c2be1459944fe636b6856c3023e1ad39f
Francis Galiegue <fgaliegue@gmail.com>
2014-02-10 21:09:44 +0100

    Revert "Remove obsolete classes"
    This reverts commit 127fcf2f7076a0d44301bdcfe83e233d134ef951.
    
    Compile fails otherwise...

---
+++ src/main/java/com/github/fge/jsonschema/loader/read/DefaultSchemaReader.java
@@ -0,0 +1,67 @@
+package com.github.fge.jsonschema.loader.read;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.github.fge.jackson.JacksonUtils;
+import com.github.fge.jsonschema.load.Dereferencing;
+import com.github.fge.jsonschema.ref.JsonRef;
+import com.github.fge.jsonschema.tree.SchemaTree;
+import com.google.common.annotations.Beta;
+import com.google.common.io.Closer;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.EnumSet;
+
+@Beta
+public final class DefaultSchemaReader
+    extends SchemaReader
+{
+    private final Dereferencing dereferencing;
+    private final ObjectReader objectReader;
+
+    @Inject
+    public DefaultSchemaReader(final Dereferencing dereferencing,
+        final EnumSet<JsonParser.Feature> parserFeatures)
+    {
+        this.dereferencing = dereferencing;
+        final ObjectMapper mapper = JacksonUtils.newMapper();
+        for (final JsonParser.Feature feature: parserFeatures)
+            mapper.configure(feature, true);
+        objectReader = mapper.reader();
+    }
+
+    @Override
+    public SchemaTree read(final JsonRef ref, final InputStream in)
+        throws IOException
+    {
+        final Closer closer = Closer.create();
+        closer.register(in);
+
+        try {
+            final JsonNode node = objectReader.readTree(in);
+            return dereferencing.newTree(ref, node);
+        } finally {
+            closer.close();
+        }
+    }
+
+    @Override
+    public SchemaTree read(final JsonRef ref, final Reader reader)
+        throws IOException
+    {
+        final Closer closer = Closer.create();
+        closer.register(reader);
+
+        try {
+            final JsonNode node = objectReader.readTree(reader);
+            return dereferencing.newTree(ref, node);
+        } finally {
+            closer.close();
+        }
+    }
+}
---
+++ src/test/java/com/github/fge/jsonschema/loader/read/DefaultSchemaReaderModuleTest.java
@@ -0,0 +1,87 @@
+package com.github.fge.jsonschema.loader.read;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.github.fge.jsonschema.messages.JsonSchemaCoreMessageBundle;
+import com.github.fge.msgsimple.bundle.MessageBundle;
+import com.github.fge.msgsimple.load.MessageBundles;
+import com.google.common.io.Closer;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static org.testng.Assert.*;
+
+public final class DefaultSchemaReaderModuleTest
+{
+    private static final MessageBundle BUNDLE
+        = MessageBundles.getBundle(JsonSchemaCoreMessageBundle.class);
+
+    @Test
+    public void defaultReaderModuleRefusesNullDereferencing()
+    {
+        try {
+            new DefaultSchemaReaderModule() {
+                {
+                    setDereferencing(null);
+                }
+            };
+            fail("No exception thrown!");
+        } catch (NullPointerException e) {
+            assertEquals(e.getMessage(),
+                BUNDLE.getMessage("loadingCfg.nullDereferencingMode"));
+        }
+    }
+
+    @Test
+    public void defaultReaderModuleRefusesNullParserOption()
+    {
+        try {
+            new DefaultSchemaReaderModule() {
+                {
+                    addParserFeature(null);
+                }
+            };
+            fail("No exception thrown!");
+        } catch (NullPointerException e) {
+            assertEquals(e.getMessage(),
+                BUNDLE.getMessage("loadingCfg.nullJsonParserFeature"));
+        }
+    }
+
+
+
+    @Test(dependsOnMethods = "defaultReaderModuleRefusesNullParserOption")
+    public void defaultReaderModuleAcknowledgesParserOptions()
+        throws IOException
+    {
+        final SchemaReaderModule module = new DefaultSchemaReaderModule() {
+            {
+                addParserFeature(JsonParser.Feature.ALLOW_COMMENTS);
+                addParserFeature(JsonParser.Feature.ALLOW_SINGLE_QUOTES);
+                addParserFeature(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES);
+            }
+        };
+
+        final SchemaReader reader = module.getReader();
+
+        final URL url = DefaultSchemaReaderModuleTest.class
+            .getResource("/load/nonstandard-source.json");
+
+        if (url == null)
+            throw new IOException("resource not found");
+
+        final Closer closer = Closer.create();
+        final InputStream in;
+
+        try {
+            in = closer.register(url.openStream());
+            reader.read(in);
+        } finally {
+            closer.close();
+        }
+        assertTrue(true);
+    }
+
+}
