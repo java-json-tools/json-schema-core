4878ba7ca66b0655badec602d5a7c5eba1502f7b
Francis Galiegue <fgaliegue@gmail.com>
2014-03-06 19:02:43 +0100

    AbstractSyntaxChecker: resolve one javadoc issue

--- src/main/java/com/github/fge/jsonschema/core/keyword/syntax/checkers/AbstractSyntaxChecker.java
+++ src/main/java/com/github/fge/jsonschema/core/keyword/syntax/checkers/AbstractSyntaxChecker.java
@@ -140,7 +140,7 @@ public abstract class AbstractSyntaxChecker
      * @param bundle the message bundle to use
      * @param key the message
      * @return a new {@link ProcessingMessage}
-     * @see ProcessingMessage#message(Object)
+     * @see ProcessingMessage#setMessage(String)
      */
     protected final ProcessingMessage newMsg(final SchemaTree tree,
         final MessageBundle bundle, final String key)
