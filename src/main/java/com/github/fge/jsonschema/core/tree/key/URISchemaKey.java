499aa5d30ff028a38f4a5b071052bdb5b5f3e11e
Francis Galiegue <fgaliegue@gmail.com>
2014-05-24 11:35:32 +0200

    Introduce SchemaKey
    There are two implementations:
    
    * one for "anonymous" schemas;
    * one for non anonymous schemas.
    
    By "anonymous" it is meant schemas not loaded via URIs.
    
    All implementations have equals()/hashCode(); anonymous schema keys are
    generated via an AtomicLong's .incrementAndGet().
    
    This means a loaded SchemaTree, even anonymous, can be identified quickly, via
    its schema key and current pointer.

---
+++ src/main/java/com/github/fge/jsonschema/core/tree/key/URISchemaKey.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of this file and of both licenses is available at the root of this
+ * project or, if you have the jar distribution, in directory META-INF/, under
+ * the names LGPL-3.0.txt and ASL-2.0.txt respectively.
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.jsonschema.core.tree.key;
+
+import com.github.fge.jsonschema.core.ref.JsonRef;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public final class URISchemaKey
+    extends SchemaKey
+{
+    URISchemaKey(final JsonRef ref)
+    {
+        super(ref);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return ref.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable final Object obj)
+    {
+        if (obj == null)
+            return false;
+        if (this == obj)
+            return true;
+        if (getClass() != obj.getClass())
+            return false;
+        final URISchemaKey other = (URISchemaKey) obj;
+        return ref.equals(other.ref);
+    }
+
+    @Nonnull
+    @Override
+    public String toString()
+    {
+        return "loaded from JSON ref " + ref;
+    }
+}
