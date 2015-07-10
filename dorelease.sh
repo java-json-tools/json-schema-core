56803054587f30dd541f642181ca46ec1d1ea522
John Huffaker <jhuffaker@box.com>
2015-07-09 17:51:40 -0700

    Add instructions to dorelease

--- dorelease.sh
+++ dorelease.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Release instructions
+# 1. Follow the gpg setup instructions: http://central.sonatype.org/pages/working-with-pgp-signatures.html
+# 2. In project root create gradle.properties and add:
+# sonatypeUsername=metadata-dev
+# sonatypePassword=password in vault for oss sonatype<ensure newline>
+#
+# signing.keyId=generated key
+# signing.password=password for gpg key
+# signing.secretKeyRingFile=/Users/username/.gnupg/secring.gpg
+# 3. ./dorelease.sh
+# 4. Go to UI at: https://oss.sonatype.org
+# 5. Login as metadata-dev
+# 6. Follow the instructions here: http://central.sonatype.org/pages/releasing-the-deployment.html
+
 ./gradlew --recompile-scripts clean test uploadArchives
 
